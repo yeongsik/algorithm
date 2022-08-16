f6c055da71dc4bd73a6647d9e2c3a42837b09412
ys <youngsik0266@gmail.com>
2022-08-16 23:41:16 +0900

    feat : 2751 정렬문제 퀵소트 구현 ( 랜덤피벗 )

--- src/baekjoon/sortnumber/QuickSort2751.java
+++ src/baekjoon/sortnumber/QuickSort2751.java
@@ -1,5 +1,6 @@
 package baekjoon.sortnumber;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class QuickSort2751 {
@@ -12,17 +13,49 @@ public class QuickSort2751 {
             i++;
         }
 
+        quickSort(intArray);
+        printIntArr(intArray);
+
     }
 
-    private void quickSort(int[] arr) {
+    private static void quickSort(int[] arr) {
         quickSort(arr , 0 , arr.length-1);
     }
 
-    private void quickSort(int[] arr, int start, int end) {
+    private static void quickSort(int[] arr, int start, int end) {
+        int partNumber = partition(arr, start, end);
 
+        if (start < partNumber-1) {
+            quickSort(arr,start,partNumber-1);
+        }
+        if (partNumber < end) {
+            quickSort(arr, partNumber, end);
+        }
     }
 
-    private void printIntArr(int[] arr) {
+    private static int partition(int[] arr, int start, int end) {
+        Random random = new Random();
+        int pivot = arr[random.nextInt(arr.length)];
+
+        while (start <= end) {
+            while ( arr[start] < pivot ) start++;
+            while ( arr[end] > pivot ) end--;
+            if (start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    private static void printIntArr(int[] arr) {
         for (int a : arr) {
             System.out.println(a);
         }
