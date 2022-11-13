23f3869b55a80526840bde426fe5a8ad0cfcde3c
kwon-yeongsik <youngsik0266@gmail.com>
2022-11-13 13:57:03 +0900

    feat : BOJ 2075 N번째 큰 수

---
+++ src/baekjoon/Main_2075.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int idx = 1;
+        while (!pq.isEmpty()) {
+            int x = pq.poll();
+            if (idx == n) {
+                System.out.println(x);
+            }
+            idx++;
+        }
+    }
+}
