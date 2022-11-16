9c50c5af472f9683e3375acd195e1f8cc229f13d
kwon-yeongsik <youngsik0266@gmail.com>
2022-11-16 11:41:43 +0900

    update : 인프런 가장 높은 탑 쌓기 - DP
    정렬 후 풀이 완료

--- src/inflearn/section10/Section10_4.java
+++ src/inflearn/section10/Section10_4.java
@@ -3,12 +3,15 @@ package inflearn.section10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Section10_4 {
     static int[] dy;
 
-    static class Rectangle {
+    static class Rectangle implements Comparable<Rectangle> {
+
 
         private int area;
         private int height;
@@ -19,12 +22,18 @@ public class Section10_4 {
             this.height = height;
             this.weight = weight;
         }
+
+        @Override
+        public int compareTo(Rectangle o) {
+            return o.area - this.area;
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Rectangle[] recArr = new Rectangle[n];
+        List<Rectangle> list = new ArrayList<>();
         dy = new int[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,14 +41,18 @@ public class Section10_4 {
             int height = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
             recArr[i] = new Rectangle(area, height, weight);
+            list.add(new Rectangle(area, height, weight));
         }
 
-        dy[0] = recArr[0].height;
+        list.sort(Rectangle::compareTo);
+
+        dy[0] = list.get(0).height;
         for (int i = 1; i < n; i++) {
-            int max = recArr[i].height;
+            int max = list.get(i).height;
+            Rectangle pointerRec = list.get(i);
             for (int j = i - 1; j >= 0; j--) {
-                if (recArr[j].area > recArr[i].area && recArr[j].weight > recArr[i].weight) {
-                    max = Math.max(max, dy[j] + recArr[i].height);
+                if (list.get(j).weight > pointerRec.weight) {
+                    max = Math.max(max, dy[j] + pointerRec.height);
                 }
             }
             dy[i] = max;
