acf1347380cfeb5f443c73cb059580b0fde5e153
kwon-yeongsik <youngsik0266@gmail.com>
2022-11-21 20:19:25 +0900

    feat : HakerRank 트리 레벨 순회

---
+++ src/hackerank/LevelOrderTraversal.java
@@ -0,0 +1,70 @@
+package hackerank;
+
+import java.util.*;
+
+/*
+    https://www.hackerrank.com/challenges/tree-level-order-traversal/problem?isFullScreen=true
+ */
+public class LevelOrderTraversal {
+
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void levelOrder(Node root) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node curNode = queue.poll();
+                System.out.print(curNode.data + " ");
+                if (curNode.left != null) {
+                    queue.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.add(curNode.right);
+                }
+            }
+        }
+
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }
+}
+
