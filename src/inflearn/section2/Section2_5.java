4f4566423521a6ffb4f95892a4f8dc5af95f1ed3
kwon-yeongsik <youngsik0266@gmail.com>
2023-01-04 10:23:38 +0900

    update : 인프런 에라토스테네스 체

--- src/inflearn/section2/Section2_5.java
+++ src/inflearn/section2/Section2_5.java
@@ -1,29 +1,33 @@
 package inflearn.section2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Section2_5 {
     static int[] ch;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
         ch = new int[n + 1];
         int cnt = 0;
         for (int i = 2; i < ch.length; i++) {
             if (ch[i] == 0) {
+                bw.write(i + " ");
                 cnt++;
-            }
-            int j = 1;
-            while (i * j <= n) {
-                ch[i * j] = 1;
-                j++;
-            }
-        }
 
-        System.out.println(cnt);
+                for (int j = i; j <= n; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
+
+        }
+        bw.write("\n");
+        bw.write(cnt + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
