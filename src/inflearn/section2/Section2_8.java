63aa94279a6d0fe3ae87781b66ceec6b7385e73d
kwon-yeongsik <youngsik0266@gmail.com>
2023-01-04 22:24:27 +0900

    feat : 인프런 등수구하기

---
+++ src/inflearn/section2/Section2_8.java
@@ -0,0 +1,38 @@
+package inflearn.section2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Section2_8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] order = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    cnt++;
+                }
+            }
+            order[i] = cnt;
+        }
+
+        for (int k : order) {
+            bw.write(k + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
